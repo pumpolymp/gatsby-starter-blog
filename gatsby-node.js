3a0cf10fa8ea7e332af4999ef6a782f679fae534
PumpOlymp <pumpolymp@gmail.com>
2021-08-23 20:42:28 +0300

    /blog

--- gatsby-node.js
+++ gatsby-node.js
@@ -113,3 +113,35 @@ exports.createSchemaCustomization = ({ actions }) => {
     }
   `)
 }
+
+const fs = require('fs-extra')
+const path = require('path')
+
+/*
+ * Gatsby Cloud passes some environment variables
+ * You can use these to only perform some moves conditionally
+ */
+const { RUNNER_TYPE = 'BUILDS' } = process.env
+const pathPrefix = `/blog`
+
+/*
+ * Note: this is a non-ideal workaround
+ * that we are encouraging until we ship pathPrefix support in Gatsby Hosting
+ * Please follow: https://gatsby.canny.io/gatsby-cloud/p/prefix-path-support
+ */
+exports.onPostBuild = async function onPostBuild() {
+  const allowList = ['BUILDS', 'INCREMENTAL_BUILDS']
+
+  if (allowList.includes(RUNNER_TYPE)) {
+    const public = path.join(__dirname, 'public')
+    const tmp = path.join(__dirname, 'public2')
+    await fs.rename(public, tmp)
+    await fs.copy(
+        tmp,
+        path.join(public, pathPrefix),
+    )
+
+    await fs.remove(tmp)
+  }
+}
+
